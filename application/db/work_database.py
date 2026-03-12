cabb7efa29dcdf1ea881a1b36ac79f07c128050c
Dmitriy Timofeev <28dngn@gmail.com>
2026-03-12 23:29:56 +0300

    сделана рекция на кнопки "практиковаться" и "дальше"

--- application/db/work_database.py
+++ application/db/work_database.py
@@ -28,3 +28,58 @@ def get_or_create_user(telegram_id):
         return None
     finally:
         conn.close()
+
+
+def get_random_word(user_id):
+    """getting a random word"""
+    conn = get_db_connection()
+    if not conn:
+        return None
+
+    try:
+        with conn.cursor() as cursor:
+            query = """
+            SELECT w.id, w.word_ru, w.word_en
+            FROM words w
+            LEFT JOIN users_words uw ON uw.word_id = w.id
+            WHERE w.is_common = TRUE 
+               OR uw.user_id = %s
+            ORDER BY RANDOM() LIMIT 1
+            """
+            cursor.execute(query, (user_id,))
+            result = cursor.fetchone()
+            return result
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+    finally:
+        conn.close()
+
+
+def get_wrong_words(correct_word_id, limit=3):
+    """Getting 3 incorrect answers."""
+    conn = get_db_connection()
+    if not conn:
+        return []
+
+    try:
+        with conn.cursor() as cursor:
+            query = """
+            SELECT w.word_en
+            FROM words w
+            WHERE w.id != %s
+            ORDER BY RANDOM() LIMIT %s
+            """
+            cursor.execute(query, (correct_word_id, limit))
+            result = cursor.fetchall()
+            return [r[0] for r in result] if result else []
+    except Exception as e:
+        print(f"Error: {e}")
+        return []
+    finally:
+        conn.close()
+
+
+# if __name__ == "__main__":
+# print(get_wrong_words(1))
+# print(get_random_word(u_id))
--- application/init_bot.py
+++ application/init_bot.py
@@ -2,10 +2,30 @@
 
 import os
 import logging
+import random
 import telebot
 from telebot import custom_filters, types
 from dotenv import load_dotenv
-from application.db.work_database import get_or_create_user
+from application.db.work_database import (
+    get_or_create_user,
+    get_random_word,
+    get_wrong_words,
+)
+from telebot.storage import StateMemoryStorage
+from telebot.states import State, StatesGroup
+
+
+class Command:
+    TEACH = "Практиковаться"
+    ADD_WORD = "Добавить новое слово"
+    DELETE_WORD = "Удалить слово"
+    NEXT = "Дальше ⏭"
+
+
+class MyStates(StatesGroup):
+    correct_en = State()
+    correct_ru = State()
+    wrong_words = State()
 
 
 load_dotenv()
@@ -13,7 +33,18 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 if not BOT_TOKEN:
     raise ValueError("Не найден токен бота! Проверьте файл .env")
 
-bot = telebot.TeleBot(BOT_TOKEN)
+# ==================================
+# used_words: dict[int, list[int]] = {}
+
+
+# def save_word(user_id, word_id):
+#     used_words[user_id].append(word_id)
+
+
+# ==================================
+
+state_storage = StateMemoryStorage()
+bot = telebot.TeleBot(BOT_TOKEN, state_storage=state_storage)
 
 
 @bot.message_handler(commands=["start"])
@@ -22,19 +53,82 @@ def begin_work(message):
     get_or_create_user(message.from_user.id)
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
 
-    btn_start = types.KeyboardButton("Начать практиковаться")
-    btn_add = types.KeyboardButton("Добавить слово")
-    btn_delete = types.KeyboardButton("Удалить слово")
+    btn_start = types.KeyboardButton(Command.TEACH)
+    btn_add = types.KeyboardButton(Command.ADD_WORD)
+    btn_delete = types.KeyboardButton(Command.DELETE_WORD)
     markup.add(btn_start, btn_add, btn_delete)
 
     welcome_text = (
         f"Привет, {message.from_user.first_name}! \n"
-        "Я бот для изучения английских слов.\n\n"
+        "Я бот для изучения английских слов.\n"
         "Выбери действие"
     )
     bot.send_message(message.chat.id, welcome_text, reply_markup=markup)
 
 
+@bot.message_handler(func=lambda message: message.text == "Практиковаться")
+def start_teach(message):
+    user_sql_id = get_or_create_user(message.from_user.id)
+    word_data = ""
+    if user_sql_id:
+        word_data = get_random_word(user_sql_id)
+
+    if not word_data:
+        bot.send_message(
+            message.chat.id,
+            "В базе данных пока нет слов, добавьте свои слова!",
+        )
+        return
+
+    try:
+        word_id, correct_ru, correct_en = word_data
+
+        limit_wrong_words = 3
+        wrong_words = get_wrong_words(word_id, limit_wrong_words)
+        if len(wrong_words) != 3:
+            while len(wrong_words) < 3:
+                wrong_words.append("FakeWord")
+
+        words_en = wrong_words.copy()
+        words_en.append(correct_en)
+
+        buttons = []
+        words_en_btns = [types.KeyboardButton(word) for word in words_en]
+        buttons.extend(words_en_btns)
+        random.shuffle(buttons)
+        next_btn = types.KeyboardButton(Command.NEXT)
+        add_word_btn = types.KeyboardButton(Command.ADD_WORD)
+        delete_word_btn = types.KeyboardButton(Command.DELETE_WORD)
+        buttons.extend([next_btn, add_word_btn, delete_word_btn])
+
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+        markup.add(*buttons)
+
+        greeting = f"Выбери перевод слова:\n🇷🇺 {correct_ru}"
+        bot.send_message(message.chat.id, greeting, reply_markup=markup)
+
+        bot.set_state(
+            message.from_user.id, MyStates.correct_en, message.chat.id
+        )
+        state_data = {
+            "correct_en": correct_en,
+            "correct_ru": correct_ru,
+            "wrong_words": wrong_words,
+            "word_id": word_id,
+        }
+        with bot.retrieve_data(message.from_user.id, message.chat.id) as data:  # type: ignore
+            data.update(state_data)
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+@bot.message_handler(func=lambda message: message.text == Command.NEXT)
+def next_cards(message):
+    start_teach(message)
+
+
+# =============================================================
 def init_bot():
     telebot.logger.setLevel(logging.ERROR)
     bot.add_custom_filter(custom_filters.StateFilter(bot))
